d078513d538c08d1afd5ef41123b2ea040e33c1b
Thibault "bui" Koechlin <orixxx@gmail.com>
2025-10-29 15:08:34 +0100

    mcpb package (#17)
    * mcpb package
    
    ---------
    
    Co-authored-by: Sebastien Blot <sebastien@crowdsec.net>

--- src/crowdsec_local_mcp/__init__.py
+++ src/crowdsec_local_mcp/__init__.py
@@ -2,4 +2,9 @@
 
 from .mcp_core import main
 
-__all__ = ["main"]
+try:
+    from ._version import __version__
+except ModuleNotFoundError:  # pragma: no cover - generated during release
+    __version__ = "0.0.0"
+
+__all__ = ["__version__", "main"]
